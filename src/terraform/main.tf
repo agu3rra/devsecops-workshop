981851ab1bd61143b7d8b35528d06c03e6e37bf1
Andre Guerra <a.guerra@sap.com>
2024-01-16 12:43:02 -0300

    working with terraform version 3.16 after some hiccupts with registering on latest; nsg and vnet provisioned

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -3,25 +3,48 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.87.0
+      version = "~> 3.16.0"
     }
   }
 
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
+# Resource Group: the thing that ties all of the resource together
 resource "azurerm_resource_group" "rg" {
   name     = "guerraVmLoggingRg"
   location = "East US"
 }
 
-# Resource VNET
+# Network Security Group
+resource "azurerm_network_security_group" "guerra_nsg" {
+  name                = "guerra-security-group"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-# Resource Network Security Group
+# Virtual Network (VNET)
+resource "azurerm_virtual_network" "guerra_vnet" {
+  name                = "guerra-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "main-subnet"
+    address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.guerra_nsg.id
+  }
+
+  tags = {
+    environment = "Testing"
+  }
+}
 
 # Resource Azure Key Vault
 
